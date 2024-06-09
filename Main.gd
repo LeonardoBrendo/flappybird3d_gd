fb3aa38795a6253f53ab627c9edcd46df86b6377
LeonardoBrendo <leonardobrendo@hotmail.com>
2024-06-09 19:20:16 -0300

    menu inicial, opções no jogo, level

--- Main.gd
+++ Main.gd
@@ -11,3 +11,8 @@ func _on_gerac_timeout():
 	novoscanos.translation.z = -33
 	add_child(novoscanos)
 	
+
+
+func _on_RigidBody_body_entered(body):
+	if body.name == "Player":
+		get_tree().reload_current_scene()
--- Player.gd
+++ Player.gd
@@ -7,3 +7,7 @@ func _physics_process(delta):
 	if Input.is_action_pressed("voa"):
 		$batendoasas.play()
 		linear_velocity.y = 300*delta
+	if Input.is_action_pressed("fechar"):
+		get_tree().quit()
+	if Input.is_action_pressed("reiniciar"):
+		get_tree().reload_current_scene()
